e86ecd833e47d0f7ce31ad1dedbfe5ae29cb4f9a
otakustay <otakustay@gmail.com>
2014-01-23 11:11:58 +0800

    完善console跟踪输出

--- src/trackers/console.js
+++ src/trackers/console.js
@@ -7,16 +7,47 @@
  */
 define(
     function (require) {
-        function log(group, message) {
-            if (window.console) {
-                if (group && console.group) {
-                    console.group(group);
+        function logLine(entries) {
+            if (!window.console) {
+                return;
+            }
+
+            // IE的`console.log`不是函数，不支持`apply`，且不支持多个参数
+            if (typeof console.log === 'function') {
+                console.log.apply(console, entries);
+            }
+            else {
+                console.log(entries.join(' '));
+            }
+        }
+
+        function logWithGroup(groupName) {
+            if (!window.console) {
+                return;
+            }
+            
+            if (console.groupCollapsed) {
+                console.groupCollapsed(groupName);
+                for (var i = 1; i < arguments.length; i++) {
+                    logLine(arguments[i]);
                 }
-
-                console.log(message);
-
-                if (group && console.group) {
-                    console.groupEnd(group);
+                console.groupEnd(groupName);
+            }
+            else {
+                console.log('➤' + groupName);
+                var prefix = '├───';
+                for (var i = 1; i < arguments.length; i++) {
+                    if (i === arguments.length - 1) {
+                        prefix = '└───';
+                    }
+                    var entry = arguments[i];
+                    if (typeof entry === 'string') {
+                        entry = prefix + entry;
+                    }
+                    else {
+                        entry[0] = prefix + entry[0];
+                    }
+                    logLine(entry);
                 }
             }
         }
@@ -27,15 +58,39 @@ define(
             return {
                 name: 'console',
 
-                trackPageView: function (context) {
-                    var message = 'Forward to "' + context.url + '"';
-                    if (context.referrer) {
-                        message += ' from "' + context.referrer + '"';
+                trackException: function (context) {
+                    var entries = [
+                        '我靠，有个Promise出异常了',
+                        ['出事的Deferred对象: ', context.deferred],
+                        ['出事时给的参数: ', context.args],
+                        ['出事的原因大概是: ' + context.reason]
+                    ];
+                    if (context.reason instanceof Error) {
+                        entries.push(
+                            '好像是一个异常对象，所以把调用堆栈给你看看好了：\n'
+                                + context.reason.stack
+                        );
                     }
+                    logWithGroup.apply(null, entries);
+                },
 
-                    log(config.account || config.group, message);
+                trackEnterAction: function (context) {
+                    logWithGroup(
+                        '亲你正在进入"' + context.url + '"',
+                        ['Action：', context.action],
+                        ['Model：', context.action.model],
+                        ['Model里的数据：', context.action.model.dump()],
+                        ['View：', context.action.view],
+                        ['DOM容器：', context.action.view.getContainerElement()]
+                    );
+                },
 
-                    return this;
+                trackLeaveAction: function (context) {
+                    logWithGroup(
+                        '亲你已经离开"' + context.action.context.url + '"',
+                        ['当前的Action：', context.action],
+                        ['前往的URL：' + context.to.url]
+                    );
                 },
 
                 load: function (callback) {
